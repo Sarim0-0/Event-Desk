9a402ca7566b54a79a3d6591c91ec4deec4b8b43
Sarim0-0 <m.sarim2410@gmail.com>
2026-08-28 01:30:30 +0500

    feat(notifications): add notificationn websocket route

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from app.api.routes.auth import router as auth_router
 from app.api.routes.booking import router as booking_router
 from app.api.routes.event import router as event_router
+from app.api.routes.notification import router as notification_router
 from app.api.routes.reply import router as reply_router
 from app.api.routes.review import router as review_router
 
@@ -13,3 +14,4 @@ api_router.include_router(booking_router)
 api_router.include_router(event_router)
 api_router.include_router(review_router)
 api_router.include_router(reply_router)
+api_router.include_router(notification_router)
